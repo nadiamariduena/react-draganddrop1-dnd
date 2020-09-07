7bae991af5e52b524927c562bd9c4946f4dc75a1
melissa <nadia.mm.mariduena@gmail.com>
2020-09-07 23:57:58 +0200

    setting up the files

---
+++ src/initial-data.js
@@ -0,0 +1,7 @@
+const initialData = {
+  tasks: {
+    "task-1": { id: "task-1", content: "Search for tutorials" },
+    "task-2": { id: "task-2", content: "watch them 2 times" },
+    "task-3": { id: "task-3", content: "then practice them" },
+  },
+};
