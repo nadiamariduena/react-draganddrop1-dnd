4f0dae3bbeb5837b772c2f34bba2a1eb934ffdaa
melissa <nadia.mm.mariduena@gmail.com>
2020-09-08 15:54:10 +0200

    Updates

---
+++ precache-manifest.a70aea54519133c7e468b0caf6cb5d67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab78b7292c5513f05391e378c62cd595",
+    "url": "/react-draganddrop1-dnd/index.html"
+  },
+  {
+    "revision": "16e1a322bbe151674eb8",
+    "url": "/react-draganddrop1-dnd/static/css/main.6a0ec048.chunk.css"
+  },
+  {
+    "revision": "9cad1f5e01f5163c212b",
+    "url": "/react-draganddrop1-dnd/static/js/2.78961c1f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-draganddrop1-dnd/static/js/2.78961c1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16e1a322bbe151674eb8",
+    "url": "/react-draganddrop1-dnd/static/js/main.b428b6a4.chunk.js"
+  },
+  {
+    "revision": "fa627957669ac8f50acf",
+    "url": "/react-draganddrop1-dnd/static/js/runtime-main.523b2dee.js"
+  }
+]);
